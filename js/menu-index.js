a3550e2b04ce8a56989f505e70e83d97a2e876d4
leon <capanofabio1@gmail.com>
2024-01-07 06:17:02 -0300

    se arreglo el movimiento entre secciones desde el menu de navegacion

--- js/menu-index.js
+++ js/menu-index.js
@@ -1,5 +1,6 @@
 const navbar = document.querySelector(".menu-navegacion")
 
+
 window.addEventListener("scroll", function () {
     window.scrollY > 100 && (navbar.style.background = `rgba(0,0,0,0.9)`);
     window.scrollY < 100 && (navbar.style.background = `transparent`);
@@ -7,26 +8,24 @@ window.addEventListener("scroll", function () {
 
 
 
-
-  document.addEventListener('DOMContentLoaded', function () {
+  window.addEventListener('load', function () {
     // Obtiene todos los enlaces del menú
     var menuLinks = document.querySelectorAll('nav a');
 
     // Añade un evento de clic a cada enlace del menú
     menuLinks.forEach(function (link) {
-        link.addEventListener('click', scrollSmooth);
-    });
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
 
-    // Función para realizar el desplazamiento suave
-    function scrollSmooth(e) {
-        e.preventDefault();
+            var targetId = this.getAttribute('href').substring(1);
+            var targetElement = document.getElementById(targetId);
 
-        var targetId = this.getAttribute('href').substring(1);
-        var targetElement = document.getElementById(targetId);
-
-        // Realiza el desplazamiento suave a la sección objetivo
-        targetElement.scrollIntoView({
-            behavior: 'smooth'
+            // Espera un breve momento antes de realizar el desplazamiento suave
+            setTimeout(function () {
+                targetElement.scrollIntoView({
+                    behavior: 'smooth'
+                });
+            }, 50); // Puedes ajustar este valor si es necesario
         });
-    }
+    });
 });
